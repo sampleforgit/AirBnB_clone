4270352cc56e9cbfc321bf53af3901b9f9b84b61
root <dmercy2015@gmail.com>
2023-01-13 01:29:16 -0800

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-""" 1. Script to start a Flask web application with 2 commands """
+"""This module uses Flask and starts a web application"""
+
 
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-        """ Returns some text. """
-            return 'Hello HBNB!'
+def hello_hbnb():
+    """Hello world for flask"""
+    return "Hello HBNB!"
 
 
-        @app.route('/hbnb', strict_slashes=False)
-        def hello():
-                """ Return other text. """
-                    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB for /hbnb"""
+    return "HBNB"
 
-                if __name__ == '__main__':
-                        app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
